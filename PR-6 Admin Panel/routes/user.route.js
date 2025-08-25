7034aa9e02faf45366ebc38b1ce2be4a5280eb72
Khushal vaghasiya <khushalvaghasiya0@gmail.com>
2025-08-25 15:34:14 +0530

    Delete PR-6 Admin Panel directory

--- PR-6 Admin Panel/routes/user.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { addUser } = require("../controller/user.controller");
-const userRouter = express.Router();
-
-userRouter.get("/adduser", addUser);
-
-module.exports = userRouter;
